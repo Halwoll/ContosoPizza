8418e4a0f799f2c59fa10c259c97bc30091eb6be
Halwoll <97443146+Halwoll@users.noreply.github.com>
2022-12-08 00:53:04 +0000

    implemented CRUD operations

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -29,9 +29,38 @@ public class PizzaController : ControllerBase
     return pizza;
   }
 
-  // POST action
+  [HttpPost]
+  public IActionResult Create(Pizza pizza)
+  {
+    PizzaService.Add(pizza);
+    return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+  }
 
-  // PUT acton
+  [HttpPut("{id}")]
+  public IActionResult Update(int id, Pizza pizza)
+  {
+    if (id != pizza.Id)
+      return BadRequest();
+    
+    var existingPizza = PizzaService.Get(id);
+    if(existingPizza is null)
+      return NotFound();
+    
+    PizzaService.Update(pizza);
 
-  // Delegate action
+    return NoContent();
+  }
+
+  [HttpDelete("{id}")]
+  public IActionResult Delete(int id)
+  {
+    var pizza = PizzaService.Get(id);
+
+    if (pizza is null)
+      return NotFound();
+    
+    PizzaService.Delete(id);
+
+    return NoContent();
+  }
 }
